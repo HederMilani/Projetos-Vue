4d667625a591b1b2354e8ff4d0c80a40ca6bb2b6
Heder Milani <hedermilani@MacBook-Air-de-Heder.local>
2022-08-07 19:34:20 -0300

    Criar lista de toDos

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div class="container grid-xs">
+      <img class="image-responsive image-logo" src="@/assets/GeekInvestLogo.png" alt="Logo Geek Invest">
+      <form @submit.prevent="addToDo(toDo)">
+        <div class="input-group">
+          <input type="text" v-model="toDo.description" class="form-input" placeholder="Novo ToDo">
+          <button class="btn btn-primary input-group-btn">Adicionar </button>
+        </div>
+      </form>
+      <div class="toDo-list">
+        <div v-for="t in toDos" :key="t.id">
+          <div class="title flex-centered">
+            <div class="title-icon">
+              <i class="icon icon-time"></i>
+            </div>
+            <div class="title-content">
+              {{  t.description  }}
+            </div>
+            <div class="title-action">
+              <button class="btn btn-link">Concluido</button>
+              <button class="btn btn-link">
+                <span class="text-error">Remover</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      toDos: [], 
+      toDo: { check: false},
+    }
+  },
+    methods: {
+      addToDo(toDo) {
+        toDo.id = Date.now();
+        this.toDos.push(toDo);
+        this.toDo = {check: false}
+      }
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style escoped>
+
+.image-logo{
+  max-width: 500px;
+  margin: 0px auto;
+}
+
+.toDo-list{
+  padding-top: 2rem;
+
 }
 </style>
