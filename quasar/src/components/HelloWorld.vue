449513ae099e382588b4e46e0f07b1a080d05617
Heder Milani <hedermilani@MacBook-Air-de-Heder.local>
2022-08-25 18:59:58 -0300

    Quasar formulario

--- quasar/src/components/HelloWorld.vue
+++ quasar/src/components/HelloWorld.vue
@@ -1,14 +1,65 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="../assets/logo.svg" style="width: 200px; height: 200px">
-  </q-page>
+  <div class="q-pa-md">
+    <q-form @submit="save">
+      <div class="row">
+        <div class="col-8 itens-left">
+          <q-input  outlined label="Nome"
+                    v-model="name"
+                    :roles="[ val => val && val.length > 0 || 'Campo Obrigatório']"/>
+        </div>
+
+        <div class="col-8 itens-left">
+          <q-input  outlined label="Endereco"
+                    v-model="address"
+                    :roles="[ val => val && val.length > 0 || 'Campo Obrigatório' ]"/>
+        </div>
+
+        <div class="col-8 text-center">
+          <br>
+          <q-btn color="primary" type="submit" label="Salvar" no-caps />
+
+        </div>
+      </div>
+    </q-form>
+  </div>
+  <br>
+  <div class="q-pa-md bg-grey-10 text-white">
+    <q-list dark bordered separator style="max-width: 318px"
+            >
+
+    </q-list>
+  </div>
 </template>
 
 <style>
 </style>
 
 <script>
+let id = 0
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data() {
+    return{
+      name: '',
+      address: '',
+      person: [{
+        id: '',
+        name: '',
+        address: ''
+      }]
+    }
+  },
+  methods: {
+    save() {
+      this.person.push({
+        id: id++,
+        name: this.name,
+        address: this.address
+      }),
+      this.name= '',
+      this.address= ''
+    }
+  }
 }
 </script>
